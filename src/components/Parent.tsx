4ec49659ff918dd77e8212e48d337b492b379ec2
Piotr Cichon <piotr.cichon@globallogic.com>
2025-09-11 07:45:28 +0200

    React app - useContext

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Child from './Child';
 import UseStateCounter from './UseStateCounter';
 import UseEffect from './UseEffect'
 import Child2 from './Child2';
+import ThemeContext from '../context/ThemeContext';
 
 const Parent: React.FC = () => {
   const message: string = 'Welcome from Parent';
@@ -13,9 +14,12 @@ const Parent: React.FC = () => {
     console.log(`Parent - Child has send: ${childData}`);
   };
 
+ const theme = useContext(ThemeContext); // "dark"
+
   return (
     <div>
       <h1>Component Parent</h1>
+      <div>Current theme: {theme}</div>
       <Child text={message} onButtonClick={handleChildClick} />
       <Child2 count={count} onButtonClick={handleChildClick}/>
       <UseStateCounter count={count} setCount={setCount}/>
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext("light"); // domyślna wartość
+export default ThemeContext;
